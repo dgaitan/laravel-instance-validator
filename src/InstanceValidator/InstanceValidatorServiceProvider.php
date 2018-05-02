e366535d97bab3ae87901cc814bc90fd51cf13a6
Tim McLeod <tim@timmcleod.com>
2018-05-02 14:38:27 -0400

    An empty collection/paginator will pass through collection_of and paginator_of validators now.

--- src/InstanceValidator/InstanceValidatorServiceProvider.php
+++ src/InstanceValidator/InstanceValidatorServiceProvider.php
@@ -50,10 +50,16 @@ class InstanceValidatorServiceProvider extends ServiceProvider
                 throw new Exception("The 'collection_of' validator requires a single type to be specified.");
             }
 
-            $isCollection = $value instanceof Collection;
+            if(!$value instanceof Collection)
+            {
+                throw new Exception("The 'collection_of' validator requires a Collection instance.");
+            }
+
+            if ($value->isEmpty()) return true;
+
             $itemIsCorrectType = $value[0] instanceof $parameters[0] || (strtolower($parameters[0]) === 'string' && is_string($value[0]));
 
-            return $isCollection && $itemIsCorrectType;
+            return $itemIsCorrectType;
         });
 
         Validator::replacer('collection_of', function ($message, $attribute, $rule, $parameters)
@@ -76,6 +82,8 @@ class InstanceValidatorServiceProvider extends ServiceProvider
             {
                 throw new Exception("The 'paginator_of' validator requires a LengthAwarePaginator instance.");
             }
+
+            if ($value->isEmpty()) return true;
 
             $itemIsCorrectType = $value->items()[0] instanceof $parameters[0] || (strtolower($parameters[0]) === 'string' && is_string($value->items()[0]));
 
